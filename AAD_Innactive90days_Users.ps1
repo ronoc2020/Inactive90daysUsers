6d0b1558c7d1560d9b55701d180dc3c99009167e
RO-NOC <69809290+ronoc2020@users.noreply.github.com>
2023-01-19 14:06:42 +0100

    Add files via upload
    This script is designed to find and manage inactive users in an Azure Active Directory environment. It begins by connecting to Azure AD using the Connect-AzureAD cmdlet. The number of days since the last logon is set to 90 and stored in the $DaysInactive variable. The $InactiveDate variable is then set to the current date minus the number of days stored in $DaysInactive.
    
    It then finds inactive users by using the Get-AzureADUser cmdlet to retrieve all users and filtering them by the LastDirSyncTime and AccountEnabled attributes. The script then exports the results to a CSV file using the Export-Csv cmdlet.
    
    Finally, it disables all inactive users by looping through the list of users and using the Set-AzureADUser cmdlet to set the AccountEnabled attribute to $false for each user.

---
+++ AAD_Innactive90days_Users.ps1
@@ -0,0 +1,18 @@
+﻿# Connect to Azure AD
+Connect-AzureAD
+
+# Set the number of days since last logon
+$DaysInactive = 90
+$InactiveDate = (Get-Date).AddDays(-$DaysInactive)
+
+# Find inactive users
+$Users = Get-AzureADUser -All $true | Where-Object {$_.LastDirSyncTime -lt $InactiveDate -and $_.AccountEnabled -eq $true} | Select-Object UserPrincipalName, DisplayName, LastDirSyncTime
+
+# Export results to CSV
+$Users | Export-Csv C:\Inactive90daysUsers.csv -NoTypeInformation
+
+# Disable Inactive Users
+ForEach ($User in $Users){
+  Set-AzureADUser -ObjectId $User.ObjectId -AccountEnabled $false
+  Get-AzureADUser -ObjectId $User.ObjectId | Select-Object UserPrincipalName, DisplayName, AccountEnabled
+}
